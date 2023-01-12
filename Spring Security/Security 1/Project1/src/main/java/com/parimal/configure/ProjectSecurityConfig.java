8ea1c8ae72d9d9c561ac9ce21e1d52e3bc1e89f4
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-01-12 15:48:54 +0530

    .

--- Spring Security/Security 1/Project1/src/main/java/com/parimal/configure/ProjectSecurityConfig.java
+++ Spring Security/Security 1/Project1/src/main/java/com/parimal/configure/ProjectSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,6 +34,12 @@ public class ProjectSecurityConfig
 		userDetailsService.createUser(admin);
 		userDetailsService.createUser(user);
 		return userDetailsService;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
